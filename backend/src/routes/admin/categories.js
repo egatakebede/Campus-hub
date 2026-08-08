8bfbd15bf59783c04465d6864578da1dbfb9e506
edlawitkibrom-cloud <edlawitkibrom@gmail.com>
2026-08-07 22:59:51 -0800

    Add category management admin endpoints

--- backend/src/routes/admin/categories.js
+++ backend/src/routes/admin/categories.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getAllCategories, createCategory } = require('../../controllers/categoryController');
+const {
+	getAllCategories,
+	createCategory,
+	updateCategory,
+} = require('../../controllers/categoryController');
 
 router.get('/', getAllCategories);
 router.post('/', createCategory);
+// TODO: add requireModerator middleware once wired by Nahom
+router.patch('/:id', updateCategory);
 
 module.exports = router;
